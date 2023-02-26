68c45c2cc8a5a14589aa4904066becba0e223028
loukhai <loukhairabia@gmail.com>
2023-02-26 13:01:16 +0100

    some recommendation

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ const printTrackers = async (data, periodSelected) => {
       <div class="tracker__head">
         <p class="tracker__activity">${ele.title}</p>
         <div class="tracker__more">
-        <svg width="21" height="5" alt="more-setting" xmlns="http://www.w3.org/2000/svg"><path d="M2.5 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Zm8 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Zm8 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Z" fill="#BBC0FF" fill-rule="evenodd"/></svg>
+        <svg width="21" height="5" xmlns="http://www.w3.org/2000/svg"><path d="M2.5 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Zm8 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Zm8 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Z" fill="#BBC0FF" fill-rule="evenodd"/></svg>
         </div>
       </div>
       <div class="tracker__body">
